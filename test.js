4ca62b0e00ce68b4bf76a990c6d759f99f74a8af
Abhishek Deora <deoraabhishek1995@gmail.com>
2017-07-01 00:20:35 +0530

    Structure changed to multiple files

--- test.js
+++ test.js
@@ -3,25 +3,36 @@ const fs = require('fs');
 const path = require('path');
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const dir = './';
+const allFilesFolder = './Playlists/';
 const masterFileName = 'master.json';
 const masterFilePath = path.join(dir, masterFileName);
 const master = fs.readFileSync(masterFilePath, 'utf-8');
-
-let mainObj;
-try{
-	mainObj = JSON.parse(master);
-} catch(e) {
-	console.log(masterFileName + ' could not be passed.', e);
-}
-
-for (var playlistName in mainObj) {
-	if (mainObj.hasOwnProperty(playlistName)) {
-		//let playlist = mainObj[playlistName];
-		mainObj[playlistName].forEach(function(video){
-			verifyVideo(video);
-		});
-	}
-}
+let allFiles =[];
+let fileContent = {};
+fs.readdir(allFilesFolder, (err, files) => {
+	files.forEach(file => {
+		let fileContentCurrent = fs.readFileSync(allFilesFolder + file, 'utf-8');
+		try{
+			fileContentCurrent = JSON.parse(fileContentCurrent);
+			//fileContent = fileContent+fileContentCurrent;
+		}
+		catch(e)
+		{
+			console.log(file+' could not be parsed.', e);
+		}
+		for(var playlistName in fileContentCurrent)
+		{
+			if(fileContentCurrent.hasOwnProperty(playlistName))
+			{
+				fileContent[playlistName] = fileContentCurrent[playlistName];
+				//fileContent = fileContentCurrent;
+				fileContentCurrent[playlistName].forEach(function(video){
+					verifyVideo(video);
+				});
+			}
+		}
+	});
+});
 
 function verifyVideo(video){
 	var url = video.url;
@@ -39,7 +50,7 @@ function verifyVideo(video){
 			the_name = JSON.parse(the_name);
 			the_name = the_name.items[0].snippet.title;
 			video.name = the_name;
-			fs.writeFile(masterFilePath, JSON.stringify(mainObj, null, "\t")); 
+			fs.writeFile(masterFilePath, JSON.stringify(fileContent, null, "\t")); 
 		}
 	};
 	x.send();
